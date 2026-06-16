21e8f6e0d45e1d9b903382085aa3eee0413ce74e
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2026-06-16 16:56:22 +0000

    fix: routing issue with the api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: false,
-  // apiUrl: '/api/v1',
-  apiUrl: 'http://localhost:3039/api/v1',
-  // apiUrl: 'https://pdj-dev-api.wiicode.tech/api',
-  // apiUrl: 'https://pdj-backoffice-dev.wiicode.tech/api',
-
+  apiUrl: '/api/v1',
 };
